a306dd068bae9d27562f9bc4b06b081164fef291
samyyuv <samyy.uv@gmail.com>
2022-08-02 15:15:02 +0200

    adding data

---
+++ src/data.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    title: "Rue de la Pompe",
+    location: "France",
+    googleMapsUrl: "https://goo.gl/maps/YjdjqHs67V5iLSZX6",
+    startDate: "21 March, 2021",
+    endDate: "12 Juin, 2021",
+    description: "Rue de la Pompe is a street in Paris, France, which was named after the pump which served water to the castle of Muette.",
+    imageUrl: "/images/france.jpg"
+  },
+  {
+    title: "Chiles en Nogada",
+    location: "Mexico",
+    googleMapsUrl: "https://goo.gl/maps/xSMcZgtGHjm2S5xr8",
+    startDate: "30 September, 2021",
+    endDate: "03 December, 2021",
+    description: "Chiles en nogada is a Mexican dish of poblano chiles stuffed with picadillo topped with a walnut-based cream sauce called nogada, pomegranate seeds and parsley, and it is typically served at room temperature. The picadillo usually contains panochera apple, sweet-milk pear and criollo peach.",
+    imageUrl: "/images/mexico.jpg"
+  },
+  {
+    title: "Wat Arun",
+    location: "Thailand",
+    googleMapsUrl: "https://goo.gl/maps/NyDPG2TJWMJ6QpAB8",
+    startDate: "07 February, 2020",
+    endDate: "17 April, 2020",
+    description: "Monumental Buddhist temple right on the river, with an iconic, ornately tiled central prang (spire).",
+    imageUrl: "/images/thailand.jpg"
+  }
+]
